98f42f6617d4f907fbda34160bddf134e71791c5
emmanueltlom <nwabuodafiemmanuel@gmail.com>
2021-06-30 22:30:43 +0100

    Home and categories page

---
+++ indexz.js
@@ -0,0 +1,33 @@
+// DOM items SELECTIONS
+
+const menuToggle = document.querySelector(".menu-toggle-btn");
+const navLinks = document.querySelector(".nav-list-items");
+const navItems = document.querySelectorAll(".lists");
+const form = document.querySelector(".form");
+
+//init for DOM manipulation
+
+let showMenu = false;
+
+menuToggle.addEventListener("click", toggle);
+
+function toggle() {
+  if (!showMenu) {
+    //the javascript toggle method can also be used here
+    menuToggle.classList.add("close");
+    navLinks.classList.add("show");
+    form.classList.add("show");
+    navItems.forEach((item) => {
+      item.classList.add("show");
+    });
+    showMenu = true;
+  } else {
+    menuToggle.classList.remove("close");
+    navLinks.classList.remove("show");
+    form.classList.remove("show");
+    navItems.forEach((item) => {
+      item.classList.remove("show");
+    });
+    showMenu = false;
+  }
+}
